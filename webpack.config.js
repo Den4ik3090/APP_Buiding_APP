700c2a36904ac665ae4ef81ce32c8e99d40331ad
Den4ik3090 <grishaevds@yandex.ru>
2026-02-01 17:23:37 +0300

    save_good_status-work

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 const path = require("path");
 
 module.exports = (env, argv) => {
@@ -8,32 +10,69 @@ module.exports = (env, argv) => {
     entry: "./src/index.js",
     output: {
       path: path.resolve(__dirname, "dist"),
-      filename: isProduction ? "[name].[contenthash].js" : "bundle.js",
+      filename: isProduction ? "[name].[contenthash:8].js" : "bundle.js",
+      chunkFilename: isProduction
+        ? "[name].[contenthash:8].chunk.js"
+        : "[name].chunk.js",
       clean: true,
+      assetModuleFilename: isProduction
+        ? "assets/[name].[contenthash:8][ext]"
+        : "assets/[name][ext]",
     },
     module: {
       rules: [
+        // JS/JSX
         {
           test: /\.(js|jsx)$/,
           exclude: /node_modules/,
           use: {
             loader: "babel-loader",
             options: {
-              presets: ["@babel/preset-env", "@babel/preset-react"],
+              presets: [
+                [
+                  "@babel/preset-env",
+                  {
+                    useBuiltIns: "usage",
+                    corejs: 3,
+                  },
+                ],
+                "@babel/preset-react",
+              ],
+              cacheDirectory: true,
             },
           },
         },
+        // CSS (отдельный файл для кэша)
         {
           test: /\.css$/,
-          use: ["style-loader", "css-loader"],
+          use: isProduction
+            ? [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"]
+            : ["style-loader", "css-loader", "postcss-loader"],
         },
+        // Изображения (WebP + AVIF)
         {
-          test: /\.(png|jpe?g|gif|svg)$/i,
-          use: [
-            {
-              loader: "file-loader",
-            },
-          ],
+          test: /\.(png|jpe?g|gif|webp|avif)$/i,
+          type: "asset/resource",
+          generator: {
+            filename: isProduction
+              ? "assets/images/[name].[contenthash:8][ext]"
+              : "assets/images/[name][ext]",
+          },
+        },
+        // Шрифты
+        {
+          test: /\.(woff|woff2|eot|ttf|otf)$/i,
+          type: "asset/resource",
+          generator: {
+            filename: isProduction
+              ? "assets/fonts/[name].[contenthash:8][ext]"
+              : "assets/fonts/[name][ext]",
+          },
+        },
+        // Inline SVG
+        {
+          test: /\.svg$/i,
+          type: "asset/inline",
         },
       ],
     },
@@ -45,23 +84,74 @@ module.exports = (env, argv) => {
               removeComments: true,
               collapseWhitespace: true,
               removeRedundantAttributes: true,
+              removeScriptTypeAttributes: true,
+              removeStyleLinkTypeAttributes: true,
+              minifyCSS: true,
+              minifyJS: true,
             }
           : false,
+        inject: true,
+      }),
+      // CSS отдельно (длинный кэш)
+      new MiniCssExtractPlugin({
+        filename: isProduction ? "[name].[contenthash:8].css" : "[name].css",
       }),
     ],
     devServer: {
-      static: path.resolve(__dirname, "dist"),
+      static: {
+        directory: path.resolve(__dirname, "dist"),
+      },
       port: 3000,
       open: true,
       hot: true,
+      historyApiFallback: true,
+      host: "0.0.0.0", // Доступно из сети
+      allowedHosts: "all",
     },
     resolve: {
-      extensions: [".js", ".jsx"],
+      extensions: [".js", ".jsx", ".css"],
+      alias: {
+        "@": path.resolve(__dirname, "src"),
+      },
     },
+    // ✅ ИСПРАВЛЕНО: В DEV splitChunks отключены полностью
     optimization: isProduction
       ? {
           minimize: true,
+          minimizer: [
+            new TerserPlugin({
+              terserOptions: {
+                compress: {
+                  drop_console: true,
+                },
+              },
+            }),
+          ],
+          splitChunks: {
+            chunks: "all",
+            cacheGroups: {
+              vendor: {
+                test: /[\\/]node_modules[\\/]/,
+                name: "vendors",
+                chunks: "all",
+                priority: 10,
+              },
+            },
+          },
         }
-      : {},
+      : {
+          // ✅ DEV MODE: NO SPLIT CHUNKS = один bundle.js
+          splitChunks: false,
+        },
+    cache: {
+      type: "filesystem",
+      buildDependencies: {
+        config: [__filename],
+      },
+    },
+    // 2026 Perf: Preload critical resources
+    performance: {
+      hints: isProduction ? "warning" : false,
+    },
   };
 };
